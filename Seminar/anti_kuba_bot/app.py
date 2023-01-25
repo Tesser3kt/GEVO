c37f3a20028d527a0ddb1d90e8d87ed6d84eb834
Adam Klepáč <djklepy@gmail.com>
2023-01-25 12:22:31 +0100

    More or less finished level generator.

--- Seminar/anti_kuba_bot/app.py
+++ Seminar/anti_kuba_bot/app.py
@@ -1,61 +1,107 @@
 from datetime import datetime as dt, timedelta as td
 import discord
+import translators as ts
+import translators.server as tss
 
 TOKEN = 'MTA2MjgyNjA5OTIyNDA5MjczMg.GoFrE0.Gxps1zynVGrmJ57fNyklU4ZBGhcPS-bjevMEdM'
-USER_ID = 865664712842018848
-CHAR_LIMIT = 100
-WORD_LIMIT = 10
-MAX_WARNINGS = 5
+USER_IDS = [688786931748044821, 939276662024056832]
+ERIC = 763720075227299870
+CHAR_LIMITS = {
+    688786931748044821: 100,
+    939276662024056832: 100
+}
+WORD_LIMITS = {
+    688786931748044821: 10,
+    939276662024056832: 10
+}
+MAX_WARNINGS = {
+    688786931748044821: 3,
+    939276662024056832: 3
+}
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 intents.members = True
+intents.reactions = True
 
 client = discord.Client(intents=intents)
 
-user = None
-chars_written = 0
-words_written = 0
-time_banned = None
-max_warnings = 0
+users = []
+chars_written = {}
+words_written = {}
+times_banned = {}
+max_warnings = {}
+eric = None
 
 
 @client.event
 async def on_ready():
-    global user
-    user = await client.fetch_user(USER_ID)
-    await user.send(f"Dear {user.name}, Your private word-counting "
-                    f"butler is ready to serve you!")
+    global user, eric
+    global chars_written
+    global words_written
+    global times_banned
+    global max_warnings
+    for id in USER_IDS:
+        try:
+            user = await client.fetch_user(id)
+            users.append(user)
+            times_banned[id] = None
+            chars_written[id] = 0
+            words_written[id] = 0
+            max_warnings[id] = 0
+            # await user.send(f"Dear {user.name}, Your private word-counting "
+            #                 f"butler is ready to serve you!")
+        except Exception as e:
+            pass
+
+    try:
+        eric = await client.fetch_user(ERIC)
+        await eric.send("Avek sa kan dan la man.")
+    except Exception as e:
+        pass
 
 
 @client.event
 async def on_message(message):
+    global users
     global chars_written
     global words_written
     global time_banned
     global max_warnings
-    if message.author == user:
-        if time_banned and dt.now() - time_banned > td(days=1):
-            chars_written = 0
-            words_written = 0
-        chars_written += len(message.content)
-        words_written += len(message.content.split())
-        if chars_written > CHAR_LIMIT:
+    author = message.author
+    if author in users:
+        id = author.id
+        if times_banned[id] and dt.now() - times_banned[id] > td(days=1):
+            chars_written[id] = 0
+            words_written[id] = 0
+            max_warnings[id] = 0
+        chars_written[id] += len(message.content)
+        words_written[id] += len(message.content.split())
+        if chars_written[id] > CHAR_LIMITS[id]:
             await message.delete()
-            chars_written -= len(message.content)
-            words_written -= len(message.content.split())
-            time_banned = dt.now()
-            if max_warnings < MAX_WARNINGS:
+            chars_written[id] -= len(message.content)
+            words_written[id] -= len(message.content.split())
+            times_banned[id] = dt.now()
+            if max_warnings[id] < MAX_WARNINGS[id]:
                 await message.channel.send(f"Dear {user.name}, You have exceeded the character limit for today!")
-                max_warnings += 1
-        if words_written > WORD_LIMIT:
+                max_warnings[id] += 1
+        if words_written[id] > WORD_LIMITS[id]:
             await message.delete()
-            chars_written -= len(message.content)
-            words_written -= len(message.content.split())
-            time_banned = dt.now()
-            if max_warnings < MAX_WARNINGS:
+            chars_written[id] -= len(message.content)
+            words_written[id] -= len(message.content.split())
+            times_banned[id] = dt.now()
+            if max_warnings[id] < MAX_WARNINGS[id]:
                 await message.channel.send(f"Dear {user.name}, You have exceeded the word limit for today!")
-                max_warnings += 1
+                max_warnings[id] += 1
+
+    if author.id == ERIC:
+        if len(message.content) > 200:
+            return
+        await message.add_reaction("🇫🇷")
+        phrase = message.content
+        translated = tss.google(phrase, from_language="cs", to_language="fr")
+        await message.reply(translated)
+
 
 client.run(TOKEN)
--- Seminar/level_generator/level_generator.py
+++ Seminar/level_generator/level_generator.py
@@ -1,11 +1,14 @@
 from enum import Enum
 from PIL import Image, ImageDraw
-from random import randint
+from random import randint, choice
 
 WIDTH = 60
 HEIGHT = 50
-BG = 0, 0, 0
-PROB_DECREASE = 5
+BG_COLOR = 0, 0, 0
+DEFAULT_CHANCE = 10000
+PROB_DECREASE = 100
+POINT_COLOR = 255, 255, 0
+STABLE_MOVES = 0
 
 
 class Direction(Enum):
@@ -16,29 +19,155 @@ class Direction(Enum):
     RIGHT = (1, 0)
 
 
+def draw_point(x: int, y: int, color: tuple[int]) -> None:
+    """ Draw a single point on the canvas as a 1x1 rectangle. """
+    draw.rectangle((x, y, x, y), fill=color)
+
+
+def distance(x1: int, y1: int, x2: int, y2: int) -> float:
+    """ Calculate the Manhattan distance between two points. """
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def can_move(x: int, y: int, direction: Direction) -> bool:
+    """ Check if it's possible to move in the given direction. """
+    return (0 <= x + direction.value[0] < WIDTH and
+            0 <= y + direction.value[1] < HEIGHT)
+
+
+def random_direction(probs: dict) -> Direction:
+    """ Choose a random direction. """
+    return choice([
+        *(Direction.UP for _ in range(probs[Direction.UP.value])),
+        *(Direction.DOWN for _ in range(probs[Direction.DOWN.value])),
+        *(Direction.LEFT for _ in range(probs[Direction.LEFT.value])),
+        *(Direction.RIGHT for _ in range(probs[Direction.RIGHT.value]))
+    ])
+
+
+def reset_direction_probs(probs: dict, direction: Direction) -> None:
+    """ Reset direction probabilities so that the given direction has 100 %.
+    """
+    probs[direction.value] = DEFAULT_CHANCE
+    for other_direction in Direction:
+        if other_direction != direction:
+            probs[other_direction.value] = 0
+
+
+def alter_probs(probs: dict, direction: Direction,
+                pref_dirs: tuple[Direction]) -> None:
+    """ Decrease probabilities of moving in the same direction and increase
+    probabilities of moving in adjacent directions. """
+
+    probs[direction.value] -= PROB_DECREASE
+
+    if (direction.value[1], direction.value[0]) in pref_dirs:
+        probs[(direction.value[1],
+               direction.value[0])] += 3 * PROB_DECREASE // 4
+        probs[(-direction.value[1],
+               -direction.value[0])] += PROB_DECREASE // 4
+    else:
+        probs[(direction.value[1],
+               direction.value[0])] += PROB_DECREASE // 4
+        probs[(-direction.value[1],
+               -direction.value[0])] += 3 * PROB_DECREASE // 4
+
+
 # create new 60x50 black canvas
-level_img = Image.new('RGB', (WIDTH, HEIGHT), BG)
+level_img = Image.new('RGB', (WIDTH, HEIGHT), BG_COLOR)
 
 # create a draw object
 draw = ImageDraw.Draw(level_img)
 
 # create probability dict (actual numbers are irrelevant for now)
 probs = {
-    Direction.UP: 25,
-    Direction.DOWN: 25,
-    Direction.LEFT: 25,
-    Direction.RIGHT: 25
+    Direction.UP.value: 0,
+    Direction.DOWN.value: 0,
+    Direction.LEFT.value: 0,
+    Direction.RIGHT.value: 0
 }
 
-# choose a random starting point on the rim
-side = randint(0, 1)  # north/south or east/west
-if side:
-    x = randint(0, WIDTH - 1)
-    y = 0 if randint(0, 1) else HEIGHT - 1
-else:
-    x = 0 if randint(0, 1) else WIDTH - 1
-    y = randint(0, HEIGHT - 1)
+# create a tuple of extremal points
+extremal_points = (
+    *((0, y) for y in range(HEIGHT)),
+    *((WIDTH - 1, y) for y in range(HEIGHT)),
+    *((x, 0) for x in range(WIDTH)),
+    *((x, HEIGHT - 1) for x in range(WIDTH))
+)
 
+# choose a random starting point on the rim
+x, y = choice(extremal_points)
+starting_point = x, y
+
+# determine directions to move away from the starting point
+preferred_directions = []
+if x >= WIDTH // 2:
+    preferred_directions.append(Direction.LEFT.value)
+else:
+    preferred_directions.append(Direction.RIGHT.value)
+
+if y >= HEIGHT // 2:
+    preferred_directions.append(Direction.UP.value)
+else:
+    preferred_directions.append(Direction.DOWN.value)
 
 # set probs to always go away from the rim
-direction = -x // abs(x), -y // abs(y)
+starting_dir = (Direction.DOWN if y == 0 else Direction.UP if y == HEIGHT - 1
+                else Direction.RIGHT if x == 0 else Direction.LEFT)
+reset_direction_probs(probs, starting_dir)
+
+# always move to the opposite side so the level is not too short
+if x == 0:
+    final_points = [(WIDTH - 1, y) for y in range(HEIGHT)]
+elif x == WIDTH - 1:
+    final_points = [(0, y) for y in range(HEIGHT)]
+elif y == 0:
+    final_points = [(x, HEIGHT - 1) for x in range(WIDTH)]
+else:
+    final_points = [(x, 0) for x in range(WIDTH)]
+
+# draw the first point
+draw_point(x, y, POINT_COLOR)
+
+# next point is definitely in direction 'starting_dir'
+x, y = x + starting_dir.value[0], y + starting_dir.value[1]
+
+# draw until the rim is reached again
+last_direction = starting_dir
+moves_in_direction = 0
+
+while (x, y) not in final_points:
+    # draw the point
+    draw_point(x, y, POINT_COLOR)
+
+    # decrease probability of moving in the same direction and increase
+    # probability of moving in adjacent directions
+    if moves_in_direction > STABLE_MOVES:
+        alter_probs(probs, last_direction, preferred_directions)
+
+    # move at least STABLE_MOVES in the same direction
+    # then choose a random direction based on the probabilities
+    # if not possible to move, keep choosing until it is
+
+    direction = random_direction(probs)
+    while not can_move(x, y, direction):
+        if probs[direction.value] == DEFAULT_CHANCE:
+            alter_probs(probs, direction, preferred_directions)
+        direction = random_direction(probs)
+
+    # move in the chosen direction
+    x, y = x + direction.value[0], y + direction.value[1]
+
+    # reset direction probabilities if direction changed
+    if direction != last_direction:
+        reset_direction_probs(probs, direction)
+        last_direction = direction
+        moves_in_direction = 0
+    else:
+        moves_in_direction += 1
+
+# draw the last point
+draw_point(x, y, POINT_COLOR)
+
+# save the image
+level_img.save('level.png')
