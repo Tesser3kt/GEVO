72efc713b5969b51fceb01382fc9931514746811
Adam Klepáč <djklepy@gmail.com>
2023-10-12 11:12:40 +0200

    new stuff about reals

--- 4.AB/hw_flowchart/random_distrib.py
+++ 4.AB/hw_flowchart/random_distrib.py
@@ -1,17 +1,24 @@
 from random import randint
 from collections import defaultdict
 
+STUDENTS = 12
+PROBLEMS = 6
+PROBLEMS_PER_STUDENT = 2
+MAX_DISTRIB = 4
+
 distributed = defaultdict(int)
-problems = [set() for _ in range(12)]
+problems = [set() for _ in range(STUDENTS)]
 
-while sum(len(p) for p in problems) < 24:
-    student = randint(1, 12)
-    while len(problems[student - 1]) >= 2:
-        student = randint(1, 12)
+while sum(len(p) for p in problems) < PROBLEMS_PER_STUDENT * STUDENTS:
+    student = randint(1, STUDENTS)
+    while len(problems[student - 1]) >= PROBLEMS_PER_STUDENT:
+        student = randint(1, STUDENTS)
 
-    problem = randint(1, 6)
-    while distributed[problem] >= 2 and problem in problems[student - 1]:
-        problem = randint(1, 6)
+    problem = list(sorted(range(1, PROBLEMS + 1),
+                   key=lambda x: distributed[x]))[0]
+    while (distributed[problem] >= MAX_DISTRIB
+           or problem in problems[student - 1]):
+        problem = randint(1, PROBLEMS)
 
     problems[student - 1].add(problem)
     distributed[problem] += 1
