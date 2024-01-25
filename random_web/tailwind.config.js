00e23f551dc69e0a8f179610ca4fe962ffb5b7e3
Adam Klepac <djklepy@gmail.com>
2024-01-25 19:24:54 +0100

    added jeprod

--- random_web/tailwind.config.js
+++ random_web/tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/forms'),
-	],
-}
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
