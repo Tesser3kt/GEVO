bcf217923351f2bbd08f124f149b948ce2c8b6c8
Adam Klepac <djklepy@pm.me>
2026-01-14 10:05:09 +0100

    pridan test na python pro 4.AB

---
+++ 4.AB/python/testy/test_zaklady_1.py
@@ -0,0 +1,33 @@
+# Úloha 1: Na konec daného seznamu "seznam" přidejte číslo 4 a vytiskněte jeho třetí prvek ZEZADU.
+seznam = [1, 5, -17, 8.3, 2]
+
+
+
+# Úloha 2: Ve slovníku "data" máte údaje o příjmech a výdajích z prodeje tří různých produktů. Vytvořte proměnnou "bilance" a uložte do ní rozdíl celkových výdajů od celkových příjmů napříč všemi produkty.
+data = {
+    "okurka": {
+        "prijmy": 331,
+        "vydaje": 67
+    },
+    "projektor": {
+        "prijmy": 8891,
+        "vydaje": 4422
+    },
+    "kniha": {
+        "prijmy": 123,
+        "vydaje": 412
+    }
+}
+
+
+
+# Úloha 3: Máte dán čas východu a západu slunce jako páry proměnných (hodina, minuta). Napište program, který dostane čas (opět jako hodinu a minutu) a rozhodne, zda je den či noc. Tedy, program vytiskne "den", když je den, a "noc", když je noc.
+
+hodina_vychodu = 7
+minuta_vychodu = 56
+
+hodina_zapadu = 16
+minuta_zapadu = 27
+
+hodina_ted = 10
+minuta_ted = 4
