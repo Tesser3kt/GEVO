8858b7812aeb616a5e24ac6876cb5494c1cef17c
Adam Klepáč <djklepy@gmail.com>
2026-01-22 13:08:01 +0100

    test z pythonu 3 pro 4.AB

---
+++ 4.AB/python/testy/test_zaklady_3.py
@@ -0,0 +1,38 @@
+# Úloha 1
+# V seznamu "namereno" jsou poslední tři hodnoty teploty.
+# 1) Přidejte na konec seznamu průměr prvních dvou hodnot.
+# 2) Odstraňte druhý prvek původního seznamu.
+# 3) Vytiskněte poslední prvek seznamu.
+namereno = [21, 25, 19]
+
+# sem doplňte řešení
+
+
+# Úloha 2
+# Máte řetězec "jmeno" a číslo "pocet".
+# Vytvořte proměnnou "visa" tak, aby vypadala přesně takto:
+# "VSTUP: " + jmeno + "!" opakované "pocet"-krát.
+# Příklad: jmeno="Adam", pocet=3 -> "VSTUP: Adam!Adam!Adam!"
+jmeno = "Adam"
+pocet = 4
+
+# sem doplňte řešení
+
+
+# Úloha 3
+# Ve slovníku "objednavka" jsou informace o platbě a dopravě.
+# Napište program, který vytiskne:
+# - "OK" pokud:
+#    a) zpusob je "karta" a stav je "zaplaceno"
+#       NEBO
+#    b) zpusob je "dobirka" a dopravce NENÍ "posta"
+# - jinak vytiskne "PROBLEM"
+objednavka = {
+    "platba": {
+        "zpusob": "dobirka",  # "karta" nebo "dobirka"
+        "stav": "nezaplaceno",  # "zaplaceno" nebo "nezaplaceno"
+    },
+    "doprava": {"dopravce": "zasilkovna"},  # "posta", "zasilkovna", "kuryr"
+}
+
+# sem doplňte řešení
